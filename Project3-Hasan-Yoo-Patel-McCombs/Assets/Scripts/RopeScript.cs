9930ec7b2ad9890bd789272fa88947578d49146c
Tj Hasan <tjhasan159@gmail.com>
2019-04-18 17:24:27 -0400

    Added up and down motion for the magnet
    Original magnet was having problems moving so I changed it out for another 3d gameobject

---
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/RopeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.S) && this.transform.localScale.y < 10)
+        {
+            this.transform.localScale += new Vector3(0, 0.1f, 0);
+        }
+
+        if(Input.GetKey(KeyCode.W) && this.transform.localScale.y > 3)
+        {
+            this.transform.localScale += new Vector3(0, -0.1f, 0);
+        }
+    }
+}
