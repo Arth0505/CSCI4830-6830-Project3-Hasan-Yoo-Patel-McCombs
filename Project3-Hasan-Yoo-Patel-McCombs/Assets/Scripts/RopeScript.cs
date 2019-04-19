1ecd85b2924b3cdb94ad9d48d99916575e825164
Tj Hasan <tjhasan159@gmail.com>
2019-04-19 11:53:57 -0400

    Created Rotation of the Crane

--- Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/RopeScript.cs
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/RopeScript.cs
@@ -13,6 +13,7 @@ public class RopeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //the values in the following 2 if statements are subject to change when we realize what size the boxes will be.
         if(Input.GetKey(KeyCode.S) && this.transform.localScale.y < 10)
         {
             this.transform.localScale += new Vector3(0, 0.1f, 0);
@@ -24,12 +25,13 @@ public class RopeScript : MonoBehaviour
         }
 
         //below values will need to be changed after the crane is made to final size
-        if(Input.GetKey(KeyCode.G) && this.transform.position.z >= 6.3)
+        if(Input.GetKey(KeyCode.G) && this.transform.position.z >= 3)
         {
             this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z -0.1f);
         }
 
-        if(Input.GetKey(KeyCode.T) && this.transform.position.z <= 6.3)
+        //for some reason, this if statement needs the z transform to be +4.0 from the actual coordinate you want to stop at.
+        if(Input.GetKey(KeyCode.T) && this.transform.position.z < 10.3)
         {
             this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.1f);
         }
