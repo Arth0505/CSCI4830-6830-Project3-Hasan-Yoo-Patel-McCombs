811290cf9b48c895f7148ef0d8f6c2176562e89c
Jordan <soccereagle935@aol.com>
2019-04-23 21:10:14 -0400

    Added another camera for the helper
    Can be controlled with the arrow keys with up and down moving forward and backwards. Z is up and x is down. You can hold space and then press a key to rotate the camera in that direction.

---
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/HelperScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            if (Input.GetKey("up"))
+            {
+                transform.Rotate(0, 0, .5f);
+            }
+
+            if (Input.GetKey("left"))
+            {
+                transform.Rotate(.5f, 0, 0);
+            }
+
+            if (Input.GetKey("right"))
+            {
+                transform.Rotate(-.5f, 0, 0);
+            }
+
+            if (Input.GetKey("down"))
+            {
+                transform.Rotate(0, 0, -.5f);
+            }
+
+            if (Input.GetKey("z"))
+            {
+                transform.Rotate(0, .5f, 0);
+            }
+
+            if (Input.GetKey("x"))
+            {
+                transform.Rotate(0, -.5f, 0);
+            }
+        }
+        else
+        {
+            if (Input.GetKey("up"))
+            {
+                transform.Translate(0, 0, .5f);
+            }
+
+            if (Input.GetKey("left"))
+            {
+                transform.Translate(.5f, 0, 0);
+            }
+
+            if (Input.GetKey("right"))
+            {
+                transform.Translate(-.5f, 0, 0);
+            }
+
+            if (Input.GetKey("down"))
+            {
+                transform.Translate(0, 0, -.5f);
+            }
+
+            if (Input.GetKey("z"))
+            {
+                transform.Translate(0, .5f, 0);
+            }
+
+            if (Input.GetKey("x"))
+            {
+                transform.Translate(0, -.5f, 0);
+            }
+        }
+    }
+}
