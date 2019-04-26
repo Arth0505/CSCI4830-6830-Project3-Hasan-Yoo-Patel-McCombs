b22185b388f527000f0ff58fb09de81dc7daed0f
Jordan935 <>
2019-04-25 21:55:19 -0400

    updated sorting scene and added tow display view (think it works for two minitors not same)
    In the sorting scene the boxes are now destroyed and the score is updated based on correct or wrong box placement. The display now shows both views in scene but switched when the headset is on/off in the build.

--- Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/ContainerScript.cs
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/ContainerScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ContainerScript : MonoBehaviour
 {
+	int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,7 @@ public class ContainerScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "green" && gameObject.tag == "greenContainer")
+		/*if(collision.gameObject.tag == "green" && gameObject.tag == "greenContainer")
         {
             Destroy(collision.gameObject, 1);
         }
@@ -40,6 +42,29 @@ public class ContainerScript : MonoBehaviour
         if (collision.gameObject.tag == "orange" && gameObject.tag == "orangeContainer")
         {
             Destroy(collision.gameObject, 1);
-        }
+        }*/
+	
+			if (collision.gameObject.tag == gameObject.tag) //correct box placement
+			{
+				if (gameObject.tag != "noDestroy")
+				{
+				Debug.Log("Correct box");
+				Destroy(collision.gameObject);
+				score = score + 1;
+				Debug.Log(score);
+				}
+			}
+
+		else //wrong box placement
+		{
+			
+			if (gameObject.tag != "noDestroy")
+			{
+				Debug.Log("Wrong box!");
+				Destroy(collision.gameObject);
+				score = score - 1;
+				Debug.Log(score);
+			}
+		}
     }
 }
