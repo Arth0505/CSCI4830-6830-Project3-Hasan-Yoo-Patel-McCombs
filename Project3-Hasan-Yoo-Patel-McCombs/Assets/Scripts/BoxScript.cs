4675d1befe4e75725a1cbaf2eec96dcb6ed33f5a
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2019-05-06 19:53:27 -0400

    Removed drop button mechanics and added haptics

--- Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/BoxScript.cs
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/BoxScript.cs
@@ -6,10 +6,10 @@ public class BoxScript : MonoBehaviour
 {
     //box refers to its own rigidbody. For some reason this.rigidbody doesnt want to play nice 
     public Rigidbody box;
-    public bool following = false;
+    bool following = false;
 
     private GameObject Magnet;
-    public DropButtonScript dropButton;
+    //public DropButtonScript dropButton;
     public AudioSource MagnetAttach;
     public AudioSource MagnetRelease;
     // Start is called before the first frame update
@@ -39,7 +39,7 @@ public class BoxScript : MonoBehaviour
         //when the magnet collides with the box, it begins to follow the magnet.
         if(collision.gameObject.name == "Magnet")
         {
-            dropButton.container = box;
+           // dropButton.container = box;
 
             MagnetAttach.Play();
             box.isKinematic = true;
---
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/HandScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OVR;
+using UnityEngine;
+
+public class HandScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionStay(Collision collision)
+	{
+		if (collision.gameObject.tag == "Lever")
+		{
+			if (this.gameObject.name == "hand_right")
+			{
+				OVRInput.SetControllerVibration(.2f, .6f, OVRInput.Controller.RTouch);
+			}
+			else if (this.gameObject.name == "hand_left")
+			{
+				OVRInput.SetControllerVibration(.2f, .6f, OVRInput.Controller.LTouch);
+
+			}
+		}
+		
+	}
+}
