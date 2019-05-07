c7cba4ea8836bd0be7946db84823d9f997e2b7ac
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2019-05-06 21:20:06 -0400

    Fixes magnet and scene 2

--- Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/ContainerScript.cs
+++ Project3-Hasan-Yoo-Patel-McCombs/Assets/Scripts/ContainerScript.cs
@@ -29,7 +29,7 @@ public class ContainerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("time: " + waitTime);
+       // Debug.Log("time: " + waitTime);
         if (isTiming == true)
         {
             time = time + Time.deltaTime; //time for level
